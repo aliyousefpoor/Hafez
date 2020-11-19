6bf0331b60417f684242756ab339e1999ccd4244
Ali Yousefpoor <Ali@Mahdis-MacBook-Pro.local>
2020-11-19 07:17:57 +0330

    Add Interpretation Feture

---
+++ app/src/main/java/com/example/hafez/omen/InterpretationFragment.kt
@@ -0,0 +1,34 @@
+package com.example.hafez.omen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.hafez.R
+
+class InterpretationFragment : Fragment() {
+    private lateinit var close: ImageView
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.interpretation_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        close = view.findViewById(R.id.closeInterpretation)
+        navController = Navigation.findNavController(view)
+
+        close.setOnClickListener {
+            navController.navigate(R.id.action_interpretationFragment_to_takeOmenFragment)
+        }
+    }
+}
--- app/src/main/java/com/example/hafez/omen/TakeOmenFragment.kt
+++ app/src/main/java/com/example/hafez/omen/TakeOmenFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.hafez.R
 
 class TakeOmenFragment : Fragment() {
@@ -14,7 +17,9 @@ class TakeOmenFragment : Fragment() {
     private lateinit var commentBubble: ImageView
     private lateinit var commentHeart: ImageView
     private lateinit var comment: EditText
-
+    private lateinit var interpretation: TextView
+    private lateinit var navController: NavController
+    var isClicked = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,6 +35,10 @@ class TakeOmenFragment : Fragment() {
         commentBubble = view.findViewById(R.id.commentBubble)
         commentHeart = view.findViewById(R.id.commentHeart)
         comment = view.findViewById(R.id.commentEditText)
+        interpretation = view.findViewById(R.id.omenInterpretation)
+        navController = Navigation.findNavController(view)
+
+
 
         commentBubble.visibility = View.GONE
         commentHeart.visibility = View.GONE
@@ -42,17 +51,26 @@ class TakeOmenFragment : Fragment() {
                 commentHeart.visibility = View.VISIBLE
                 comment.visibility = View.VISIBLE
             } else {
-                showBubble.setImageResource(R.drawable.here)
+//                showBubble.setImageResource(R.drawable.here)
                 commentBubble.visibility = View.GONE
                 commentHeart.visibility = View.GONE
                 comment.visibility = View.GONE
-                commentHeart.setImageResource(R.drawable.favorite)
-                comment.text.clear()
+//                commentHeart.setImageResource(R.drawable.favorite)
             }
         }
 
         commentHeart.setOnClickListener {
-            commentHeart.setImageResource(R.drawable.favorite_pressed)
+            isClicked = if (!isClicked) {
+                commentHeart.setImageResource(R.drawable.favorite_pressed)
+                true
+            } else {
+                commentHeart.setImageResource(R.drawable.favorite)
+                false
+            }
+        }
+
+        interpretation.setOnClickListener {
+            navController.navigate(R.id.action_takeOmenFragment_to_interpretationFragment)
         }
     }
 }
